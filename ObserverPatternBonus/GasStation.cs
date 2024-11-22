ca082b2a286156b4271ec3dedfa46ae35b18ccd9
Pernille Lindenskov <pernillelindenskov6@gmail.com>
2024-11-22 21:42:07 +0100

    Correct access modifiers and delete unused code

--- ObserverPatternBonus/GasStation.cs
+++ ObserverPatternBonus/GasStation.cs
@@ -3,14 +3,14 @@
     public class GasStation : IObserver
     {
         private GasCompany gasCompany;
-        public string City { get; set; }
-        public Region Region { get; set; }
+        private string city;
+        private Region region;
 
         public GasStation(GasCompany gasCompany, string city, Region region)
         {
             this.gasCompany = gasCompany;
-            City = city;
-            Region = region;
+            this.city = city;
+            this.region = region;
         }
 
         public void Update()
@@ -19,10 +19,10 @@
             double hydro = gasCompany.Price * 1.1;
             double alco = gasCompany.Price * 0.9;
 
-            switch (Region.Name)
+            switch (region.Name)
             {
                 case "Jylland":
-                    Console.WriteLine($"{gasCompany.Name}'s tankstation i {City}, {Region.Name} har modtaget følgende priser: \n" +
+                    Console.WriteLine($"{gasCompany.Name}'s tankstation i {city}, {region.Name} har modtaget følgende priser: \n" +
                         $"KeroOxygen: {kero.ToString("#.##")}\n" +
                         $"HydroOxygen: {hydro.ToString("#.##")}\n" +
                         $"AlcoOxygen: {alco.ToString("#.##")}\n");
@@ -31,7 +31,7 @@
                     kero = kero * 0.95;
                     hydro = hydro * 0.95;
                     alco = alco * 0.95;
-                    Console.WriteLine($"{gasCompany.Name}'s tankstation i {City}, {Region.Name} har modtaget følgende priser: \n" +
+                    Console.WriteLine($"{gasCompany.Name}'s tankstation i {city}, {region.Name} har modtaget følgende priser: \n" +
                         $"KeroOxygen: {kero.ToString("#.##")}\n" +
                         $"HydroOxygen: {hydro.ToString("#.##")}\n" +
                         $"AlcoOxygen: {alco.ToString("#.##")}\n");
@@ -40,7 +40,7 @@
                     kero = kero * 1.05;
                     hydro = hydro * 1.05;
                     alco = alco * 1.05;
-                    Console.WriteLine($"{gasCompany.Name}'s tankstation i {City}, {Region.Name} har modtaget følgende priser: \n" +
+                    Console.WriteLine($"{gasCompany.Name}'s tankstation i {city}, {region.Name} har modtaget følgende priser: \n" +
                        $"KeroOxygen: {kero.ToString("#.##")}\n" +
                        $"HydroOxygen: {hydro.ToString("#.##")}\n" +
                        $"AlcoOxygen: {alco.ToString("#.##")}\n");
--- ObserverPatternBonus/Region.cs
+++ ObserverPatternBonus/Region.cs
@@ -2,26 +2,11 @@
 {
     public class Region
     {
-        public string Name { get; set; }
-        //public List<string> Cities { get; set; } = new List<string>();
+        public string Name { get; }
 
         public Region(string name)
         {
             Name = name;
         }
-
-        //public List<string> Fyn { get; set; } = new List<string>
-        //{
-        //    "Odense",
-        //    "Middelfart",
-        //    "Svendborg"
-        //};
-
-        //public List<string> Sjælland { get; set; } = new List<string>
-        //{
-        //    "Korsør",
-        //    "København",
-        //    "Roskilde"
-        //};
     }
 }
